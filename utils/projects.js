6bd1725b92fd4d8085f861aaf920eaf739e85ab2
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-06-14 16:12:04 +0200

    Generate path

--- utils/projects.js
+++ utils/projects.js
@@ -3,6 +3,7 @@ import projectsData from '../strings/projects'
 for (let project of projectsData) {
   project.id = project.city.toLowerCase()
   project.image = `./${project.id}.jpg`
+  project.path = project.author.split(' ')[0].toLowerCase()
 }
 
 export default projectsData
